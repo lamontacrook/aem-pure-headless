a8095b757dede0ab09b1beb8e7fcdee39ecc14cd
Lamont Crook <lamont@adobe.com>
2023-10-19 14:35:01 -0600

    update

--- src/utils/context.js
+++ src/utils/context.js
@@ -4,7 +4,7 @@ import BrokenImage from '../media/broken.jpg';
 const accessToken = 'https://20409-gqldemo202212-stage.adobeioruntime.net/api/v1/web/gql-demo-jwt/service-credentials';
 const defaultEndpoint = '/content/_cq_graphql/aem-demo-assets/endpoint.json';
 const defaultProject = 'gql-demo-template'; //'wknd-headless';
-const defaultServiceURL = 'https://author-p24020-e1129912.adobeaemcloud.com/'; //'https://publish-p101152-e938206.adobeaemcloud.com/';
+const defaultServiceURL = 'https://publish-p24020-e1129912.adobeaemcloud.com/'; //'https://publish-p101152-e938206.adobeaemcloud.com/';
 const defaultPlaceholdersExtensionURL = 'https://1154643-geoipplaceholders.adobeio-static.net/api/v1/web/geoip-placeholders';
 
 export const AppContext = createContext({
