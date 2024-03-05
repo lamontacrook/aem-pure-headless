c9ade89b21b6f50f989cc0e193f9821d582f0baf
Lamont Crook <lamont@adobe.com>
2024-03-05 12:10:32 -0700

    update service worker

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = './service-worker.js';
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
@@ -47,6 +47,7 @@ export function register(config) {
         });
       } else {
         // Is not localhost. Just register service worker
+        console.log(swUrl);
         registerValidSW(swUrl, config);
       }
     });
