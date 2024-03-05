e71e09039d3f52a36c03df896bf3d5d7428bc2f8
Lamont Crook <lamont@adobe.com>
2024-03-05 12:02:55 -0700

    update service worker

--- src/index.js
+++ src/index.js
@@ -21,3 +21,4 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 registerServiceWorker();
+// serviceWorkerRegistration.register();
--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -32,7 +32,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = './service-worker.js';
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
@@ -105,10 +105,7 @@ function checkValidServiceWorker(swUrl, config) {
     .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get('content-type');
-      if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf('javascript') === -1)
-      ) {
+      if (response.status === 404) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
