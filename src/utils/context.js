2d281ff369de3ceefe758431b4a1b77a724e4e1f
Lamont Crook <lamont@adobe.com>
2024-04-26 18:23:48 -0600

    update context

--- src/utils/context.js
+++ src/utils/context.js
@@ -3,8 +3,8 @@ import BrokenImage from '../media/broken.jpg';
 
 const accessToken = 'https://20409-gqldemo202212-stage.adobeioruntime.net/api/v1/web/gql-demo-jwt/service-credentials';
 const defaultEndpoint = '/content/_cq_graphql/aem-demo-assets/endpoint.json';
-const defaultProject = 'gql-demo-template'; //'wknd-headless';
-const defaultServiceURL = 'https://publish-p24020-e1129912.adobeaemcloud.com/'; //'https://publish-p101152-e938206.adobeaemcloud.com/';
+const defaultProject = 'wknd-headless';
+const defaultServiceURL = 'https://publish-p101152-e938206.adobeaemcloud.com/';
 const defaultPlaceholdersExtensionURL = 'https://1154643-geoipplaceholders.adobeio-static.net/api/v1/web/geoip-placeholders';
 const {host} = location;
 const defaultVersion = (host === 'aem-pure-headless.vercel.app' || host === 'localhost:3000') ? 'v1' : 'v2';
